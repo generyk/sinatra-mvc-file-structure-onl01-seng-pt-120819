7064b95e385beb9fdc5882a1fd62cd4a9a3d1359
Eryk Rachwal <erykrachwal@yahoo.com>
2020-04-08 01:55:40 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,7 +1,7 @@
 class Dog
     attr_accessor :name, :breed, :age
 
-    @@all_dogs = []
+    @@all = []
 
     def initialize(name, breed, age)
         @name = name
@@ -11,7 +11,7 @@ class Dog
     end
 
     def self.all
-        @@all_dogs
+        @@all
     end
 
 end
